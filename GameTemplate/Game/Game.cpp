298996473b233583d3f991e43cb5f496b1030e90
gimute <y4k2t36s2416@gmail.com>
2024-04-06 15:52:58 +0900

    一応車も表示しておく

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -5,9 +5,20 @@ Game::Game()
 {
 	m_modelrenderA.Init("Assets/modelData/background/proto_map/proto_map.tkm");
 	m_modelrenderB.Init("Assets/modelData/player/proto_player/proto_player.tkm");
+
+
+	m_modelrenderC.Init("Assets/modelData/car/proto_car/proto_car.tkm");
+	m_modelrenderC.SetPosition(30.0f, 0.0f, 300.0f);
+	m_modelrenderC.Update();
 }
 
 Game::~Game()
+{
+
+}
+
+
+void Game::Update()
 {
 
 }
@@ -16,4 +27,5 @@ void Game::Render(RenderContext& rc)
 {
 	m_modelrenderA.Draw(rc);
 	m_modelrenderB.Draw(rc);
+	m_modelrenderC.Draw(rc);
 }
--- GameTemplate/Game/Game.h
+++ GameTemplate/Game/Game.h
@@ -4,10 +4,12 @@ class Game : public IGameObject
 public:
 	Game();
 	~Game();
+
+	void Update();
 	void Render(RenderContext& rc);
 private:
 	ModelRender m_modelrenderA;
 	ModelRender m_modelrenderB;
-
+	ModelRender m_modelrenderC;
 };
 
