7b82d0000e0cc913a69775481fd9d4791d0529aa
takeda haruse <ca01234015@st.kawahara.ac.jp>
2024-07-08 11:02:31 +0900

    修正

--- GameTemplate/Game/GameOver.h
+++ GameTemplate/Game/GameOver.h
@@ -12,13 +12,18 @@ public:
 	void Update();
 	//描画処理
 	void Render(RenderContext& rc);
+	//透過処理
+	void AlphaCalc();
 
 	Game* m_game = nullptr;
 	Load* m_load = nullptr;
 
 	//メンバ変数
 	SpriteRender m_spriteRender;	//スプライトレンダー
+	SpriteRender m_pressRender;
 	SoundSource* m_overBgm;			//ゲームオーバーのBGM
 	bool m_isWaitFadeout = false;
+	float m_alpha = 0.0f;
+	bool m_alphaCalcBool = false;
 };
 
